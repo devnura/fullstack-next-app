aa602f9f1d6a68159858e600ebee044b767a0c03
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2023-03-02 13:17:31 +0700

    add react query

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import AddPost from "./components/AddPosts"
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-lg py-5">Hello Devnura</h1>
+      <AddPost />
     </main>
   )
 }
